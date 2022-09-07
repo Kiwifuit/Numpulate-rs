e94f4873bfe4e766780f8216165a23a68dd0f870
Kiwifuit <mahkiwi123@gmail.com>
2022-09-07 20:10:05 +0800

    Created safe functions for C binding, added testing

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,31 @@ extern "C" {
     fn reverse(i: isize) -> isize;
 }
 
+pub fn negate_number(i: isize) -> isize {
+    unsafe { negate(i) }
+}
+
+pub fn reverse_number(i: isize) -> isize {
+    unsafe { reverse(i) }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn number_negated() {
+        let pos = 32;
+        let neg = -30;
+
+        assert_eq!(negate_number(pos), -32);
+        assert_eq!(negate_number(neg), 30);
+    }
+
+    #[test]
+    fn num_reversed() {
+        let rev = 931;
+
+        assert_eq!(reverse_number(rev), 139);
+    }
 }
